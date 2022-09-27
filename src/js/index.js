d442823448b0f63e8f68ccc972487b1c19d197b5
CatVine <catriona.vine@stormid.com>
2022-09-27 10:27:34 +0100

    Seperate Lists

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,11 @@
 	console && console.log('%c careers@stormid.com ', 'background: #272727; color: #ffffff');
 }
 
-const toDoList = document.getElementsByTagName("LI");
-for (let i = 0; i < toDoList.length; i++) {
-  const span = document.createElement("SPAN");
-  const hideButton = document.createTextNode("\u00D7");
-  span.className = "hide";
-  span.appendChild(hideButton);
-  toDoList[i].appendChild(span);
-}
-
-const hide = document.getElementsByClassName("hide");
-for (let i = 0; i < hide.length; i++) {
-	hide[i].onclick = function() {
-		let div = this.parentElement;
-		div.style.display = "none";
-	}
-}
-
 function handleError(err) {
 	console.log('oops!');
 	console.log(err);
 }
 
-const toDo = document.querySelector(".toDoItem")
 const tasks = "http://localhost:4000/api/todo";
 const taskData = fetch(tasks);
 
@@ -40,16 +22,39 @@ taskData.then((response) => response.json())
 		const label = document.createElement("LABEL");
 		label.textContent = `${data[i].title}`;
 		label.setAttribute("for", input.id);
+		if (data[i].isDone == 1) {
+			label.classList.add('todo-list__item--checked')
+			input.setAttribute("checked", "checked");
+		} else {
+			label.classList.add('todo-list--unchecked');
+		}
 		listItem.appendChild(input);
 		listItem.appendChild(label);
-		const list = document.getElementById("to-do-list");
-		list.appendChild(listItem);
+		const listIncomplete = document.getElementById("to-do-list");
+		const listComplete = document.getElementById("to-do-list--complete");
+		if (data[i].isDone == "1") {
+			listComplete.appendChild(listItem);
+		} else {
+			listIncomplete.appendChild(listItem);
+		}
 	}
 })
 .catch(handleError);
 
-// toDo.textContent = `${data[0].title}`;
-// console.log(data);
-// 	console.log(data[0].title);
-// 	console.log(data[1].title);
-// 	console.log(data[2].title);
+const toDoList = document.getElementsByTagName("LABEL");
+console.log(toDoList);
+for (let i = 0; i < toDoList.length; i++) {
+  const span = document.createElement("SPAN");
+  const hideButton = document.createTextNode("\u00D7");
+  span.className = "hide";
+  span.appendChild(hideButton);
+  toDoList[i].appendChild(span);
+}
+
+const hide = document.getElementsByClassName("hide");
+for (let i = 0; i < hide.length; i++) {
+	hide[i].onclick = function() {
+		let div = this.parentElement;
+		div.style.display = "none";
+	}
+}
