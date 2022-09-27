c2483a2762f7453b479c1e073b4ee002e5a0c0a3
CatVine <catriona.vine@stormid.com>
2022-09-27 16:25:53 +0100

    Function Tweaks

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,26 @@ function handleError(err) {
 	console.log(err);
 }
 
+function addDelete(input) {
+	// const toDoList = Array.from(document.getElementsByTagName("li"));
+	// for (let i = 0; i < toDoList.length; i++) {
+		const span = document.createElement("SPAN");
+		const hideButton = document.createTextNode("\u00D7");
+		span.className = "hide";
+		span.appendChild(hideButton);
+		input.appendChild(span);
+	}
+
+function hideOnClick() {
+	const hide = document.getElementsByClassName("hide");
+	for (let i = 0; i < hide.length; i++) {
+		hide[i].onclick = function() {
+			let div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
+
 // retrieve tasks from api
 
 const tasks = "http://localhost:4000/api/todo";
@@ -20,50 +40,57 @@ const responseData = taskData.then((response) => response.json());
 
 // create task elements
 
-responseData.then((data) => {
-	for (let i = 0; i < data.length; i++) {
-		const listItem = document.createElement("LI");
-		listItem.classList.add('todo-list__item-container');
-		const input = document.createElement("INPUT");
-		input.type = "checkbox";
-		input.classList.add("checkbox")
-		input.id = data[i].id;
-		const label = document.createElement("LABEL");
-		label.textContent = `${data[i].title}`;
-		label.setAttribute("for", input.id);
-		if (data[i].isDone == 1) {
-			label.classList.add('todo-list__item--checked')
-			input.setAttribute("checked", "checked");
-		} else {
-			label.classList.add('todo-list--unchecked');
-		}
-		listItem.appendChild(input);
-		listItem.appendChild(label);
-		const listIncomplete = document.getElementById("to-do-list");
-		const listComplete = document.getElementById("to-do-list--complete");
-		if (data[i].isDone == "1") {
-			listComplete.appendChild(listItem);
-		} else {
-			listIncomplete.appendChild(listItem);
-		}
-	}
-})
-.catch(handleError);
+// function createListItem() {
+// 	const listItem = document.createElement("LI");
+// 	listItem.classList.add('todo-list__item-container');
+// }
 
-const toDoList = Array.from(document.getElementsByTagName("li"));
-console.log(toDoList); 
-for (let i = 0; i < toDoList.length; i++) {
-	const span = document.createElement("SPAN");
-	const hideButton = document.createTextNode("\u00D7");
-	span.className = "hide";
-	span.appendChild(hideButton);
-	toDoList[i].appendChild(span);
+// function createInput() {
+// 	const input = document.createElement("INPUT");
+// 			input.type = "checkbox";
+// 			input.classList.add("checkbox")
+// 			input.id = element[i].id;
+// }
+
+// function createLabel(element, i) {
+// 	const label = document.createElement("LABEL");
+// 			label.textContent = `${element[i].title}`;
+// 			label.setAttribute("for", input.id);
+// }
+
+
+function createElementsFromAPI() {
+	responseData.then((data) => {
+		for (let i = 0; i < data.length; i++) {
+			const listItem = document.createElement("LI");
+			listItem.classList.add('todo-list__item-container');
+			const input = document.createElement("INPUT");
+			input.type = "checkbox";
+			input.classList.add("checkbox")
+			input.id = data[i].id;
+			const label = document.createElement("LABEL");
+			label.textContent = `${data[i].title}`;
+			label.setAttribute("for", input.id);
+			if (data[i].isDone == 1) {
+				label.classList.add('todo-list__item--checked')
+				input.setAttribute("checked", "checked");
+			} else {
+				label.classList.add('todo-list--unchecked');
+			}
+			listItem.appendChild(input);
+			listItem.appendChild(label);
+			const listIncomplete = document.getElementById("to-do-list");
+			const listComplete = document.getElementById("to-do-list--complete");
+			if (data[i].isDone == "1") {
+				listComplete.appendChild(listItem);
+			} else {
+				listIncomplete.appendChild(listItem);
+			}
+			addDelete(listItem);
+			hideOnClick();
+		}
+	})
+	.catch(handleError);
 }
 
-const hide = document.getElementsByClassName("hide");
-for (let i = 0; i < hide.length; i++) {
-	hide[i].onclick = function() {
-		let div = this.parentElement;
-		div.style.display = "none";
-	}
-}
+createElementsFromAPI();
